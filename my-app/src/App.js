ac90d7f1346f3dc1e652e4469415721de140b48f
Deokhaeng Lee <jefflee.ldh@gmail.com>
2020-05-23 20:53:04 +0900

    Day 5

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -17,52 +17,55 @@ const movieImages = [
   "https://otakubrit.files.wordpress.com/2016/11/b2a0423bed98d99f1ea38ceda757ffd5.jpg"
 ]
 
-const movies = [
-  {
-    title : "Your Name",
-    poster : "https://img1.daumcdn.net/thumb/R800x0/?scode=mtistory2&fname=https%3A%2F%2Fk.kakaocdn.net%2Fdn%2Fdgcuvl%2FbtqDOkOM8ti%2FJYmZXgz1T5K6i4Ch3gIQs1%2Fimg.jpg"
-  },
-
-  {
-    title : "Lord of the Rings",
-    poster:  "https://img1.daumcdn.net/thumb/R800x0/?scode=mtistory2&fname=https%3A%2F%2Fk.kakaocdn.net%2Fdn%2Fdgcuvl%2FbtqDOkOM8ti%2FJYmZXgz1T5K6i4Ch3gIQs1%2Fimg.jpg"
-  },
-
-  {
-    title : "Your Lie in april",
-    poster : "https://img1.daumcdn.net/thumb/R800x0/?scode=mtistory2&fname=https%3A%2F%2Fk.kakaocdn.net%2Fdn%2Fdgcuvl%2FbtqDOkOM8ti%2FJYmZXgz1T5K6i4Ch3gIQs1%2Fimg.jpg"
-  },
-
-  {
-    title : "Star Wars",
-    poster: "https://img1.daumcdn.net/thumb/R800x0/?scode=mtistory2&fname=https%3A%2F%2Fk.kakaocdn.net%2Fdn%2Fdgcuvl%2FbtqDOkOM8ti%2FJYmZXgz1T5K6i4Ch3gIQs1%2Fimg.jpg"
-  }
-
-]
-
 class App extends React.Component  {
 
   state = {
-    greeting: 'Hello!'
   }
 
   componentDidMount(){ //component가 render 성공적으로 되면 이게 실행됨 
-    setTimeout(() => { 
-      this.setState(
-        {
-          greeting: 'Hello Again'
-        }
-      )
+    setTimeout(() => {
+      this.setState({
+        movies: [
+          {
+            title : "Your Name",
+            poster : "https://img1.daumcdn.net/thumb/R800x0/?scode=mtistory2&fname=https%3A%2F%2Fk.kakaocdn.net%2Fdn%2Fdgcuvl%2FbtqDOkOM8ti%2FJYmZXgz1T5K6i4Ch3gIQs1%2Fimg.jpg"
+          },
+        
+          {
+            title : "Lord of the Rings",
+            poster:  "https://img1.daumcdn.net/thumb/R800x0/?scode=mtistory2&fname=https%3A%2F%2Fk.kakaocdn.net%2Fdn%2Fdgcuvl%2FbtqDOkOM8ti%2FJYmZXgz1T5K6i4Ch3gIQs1%2Fimg.jpg"
+          },
+        
+          {
+            title : "Your Lie in april",
+            poster : "https://img1.daumcdn.net/thumb/R800x0/?scode=mtistory2&fname=https%3A%2F%2Fk.kakaocdn.net%2Fdn%2Fdgcuvl%2FbtqDOkOM8ti%2FJYmZXgz1T5K6i4Ch3gIQs1%2Fimg.jpg"
+          },
+        
+          {
+            title : "Star Wars",
+            poster: "https://img1.daumcdn.net/thumb/R800x0/?scode=mtistory2&fname=https%3A%2F%2Fk.kakaocdn.net%2Fdn%2Fdgcuvl%2FbtqDOkOM8ti%2FJYmZXgz1T5K6i4Ch3gIQs1%2Fimg.jpg"
+          },
+    
+          {
+            title: "Kingdom",
+            poster: "https://img1.daumcdn.net/thumb/R800x0/?scode=mtistory2&fname=https%3A%2F%2Fk.kakaocdn.net%2Fdn%2Fdgcuvl%2FbtqDOkOM8ti%2FJYmZXgz1T5K6i4Ch3gIQs1%2Fimg.jpg"
+          }
+        ]
+      })
     }, 5000)
+  }
+
+  _renderMovies = () => {
+    const movies = this.state.movies.map((movie, index) => {
+      return <Movie title={movie.title} poster={movie.poster} key={index}/> 
+    })
+    return movies
   }
 
   render() {
   return (
     <div className="App">
-      {this.state.greeting}
-     {movies.map((movie, index) => {
-       return <Movie title={movie.title} poster={movie.poster} key={index}/> 
-     })}
+     {this.state.movies ? this._renderMovies() : 'Loading'}
     </div>
   );
   }
