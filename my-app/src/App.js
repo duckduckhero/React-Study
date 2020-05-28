c19080b41a3aaf54875d2bc6ff70732743759ff2
Deokhaeng Lee <jefflee.ldh@gmail.com>
2020-05-28 16:26:11 +0900

    async 랑 await 추가함

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -23,17 +23,29 @@ class App extends React.Component  {
   }
 
   componentDidMount(){ //component가 render 성공적으로 되면 이게 실행됨 
-    fetch("https://yts.mx/api/v2/list_movies.json?quality=3D?sort_by=rating")
-    .then(response => response.json()) //위의 라인이 완료가 되면 여기에 적힌걸 수행해라 
-    .then(json=> console.log(json))
-    .catch(err => console.log(err)) //만약 맨 위의 것이 에러가 생긴다면 에러를 출력해라. 
+   this._getMovies();
   }
 
   _renderMovies = () => {
-    const movies = this.state.movies.map((movie, index) => {
-      return <Movie title={movie.title} poster={movie.poster} key={index}/> 
+    const movies = this.state.movies.map((movie) => {
+      return <Movie title={movie.title} poster={movie.large_cover_image} key={movie.id}/> 
     })
     return movies
+  }
+
+   _getMovies = async () => {
+    //비동기적인 함수이다. 이전 작업이 끝나야 그 다음 작업이 시작하는 형태가 아닌 것이다. 
+    const movies = await this._callApi() //callApi 를 await mode 에서 movies 는 값으로 가지고 있음 
+    this.setState({
+      movies
+    })
+  }
+
+  _callApi = () => {
+    return fetch("https://yts.mx/api/v2/list_movies.json?quality=3D?sort_by=rating")
+    .then(response => response.json()) //위의 라인이 완료가 되면 여기에 적힌걸 수행해라 
+    .then(json=> json.data.movies)
+    .catch(err => console.log(err)) //만약 맨 위의 것이 에러가 생긴다면 에러를 출력해라. 
   }
 
   render() { 
