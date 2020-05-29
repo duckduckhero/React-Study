c3e396a8a420abfd2e23ee761c2c9caef4743050
Deokhaeng Lee <jefflee.ldh@gmail.com>
2020-05-30 01:00:35 +0900

    시놉시스 장르 props 를 movie 에 추가하고 예쁘게 배치함

--- my-app/src/Movie.js
+++ my-app/src/Movie.js
@@ -2,19 +2,32 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './Movie.css';
 
-function Movie({title, poster})
+function Movie({title, poster, genres, synopsis})
 {
     return(
-        <div>
-           <MoviePoster poster={poster}/>
-           <h1>{title}</h1> 
+        <div className="Movie">
+            <div className="Movie_Columns">
+                <MoviePoster poster={poster}/>
+            </div>
+            <div className="Movie_Columns">
+                <h1>{title}</h1>
+                <div className="Movie_Genres">
+                    {genres.map((genre, index) => <MovieGenre genre={genre} key={index}/>)}
+                </div>
+                <p className="Movie_Synopsis">
+                    {synopsis} 
+                </p>
+            </div>
+          
         </div>
     )
 }
 
 Movie.propTypes = {
     title: PropTypes.string.isRequired,
-    poster: PropTypes.string.isRequired
+    poster: PropTypes.string.isRequired,
+    genres: PropTypes.array.isRequired,
+    synopsis : PropTypes.string.isRequired
 }
 
 
@@ -25,8 +38,18 @@ function MoviePoster({poster}) {
     
 }
 
+function MovieGenre({genre}){
+    return(
+        <span className="Movie_Genre">{genre} </span>
+    )
+}
+
 MoviePoster.propTypes = { 
     poster: PropTypes.string.isRequired
+}
+
+MovieGenre.propTypes = {
+    genres: PropTypes.string.isRequired
 }
 
 export default Movie
